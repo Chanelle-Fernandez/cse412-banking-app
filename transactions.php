3cb151a762dbebd99e75346a19529f855b198127
Chanelle-Fernandez <44142888+Chanelle-Fernandez@users.noreply.github.com>
2022-04-26 10:58:17 -0700

    Update transactions.php

--- transactions.php
+++ transactions.php
@@ -21,19 +21,18 @@
 		$val_array = array($_SESSION["account_number"]);
 		var_dump($val_array);		
 
-		$sql = "INSERT INTO transaction (account_number) VALUES ($1)";
+		$sql = "INSERT INTO transaction (account_number) VALUES ($1) RETURNING transaction_id";
 		$query = pg_query_params($db_conn, $sql, $val_array);
 
-		//updating deposit table
-		//$trans_id = pg_query_params($db_conn, "SELECT MAX(transaction_id) FROM transaction");
+		//updating deposit table;
+		$row = pg_fetch_row($query);
+		$trans_id = $row['0'];
 
-		//$val_array = array($amount, $trans_id);
-		//var_dump($val_array);		
+		$val_array = array($amount, $trans_id);
+		var_dump($val_array);		
 
-		//$sql = "INSERT INTO transaction (deposited, transaction_id) VALUES ($1, $2)";
-		//$query = pg_query_params($db_conn, $sql, $val_array);
-
-		//echo pg_last_error();
+		$sql = "INSERT INTO deposit (deposited, transaction_id) VALUES ($1, $2)";
+		$query = pg_query_params($db_conn, $sql, $val_array);
 		
 		//updating the account
 
@@ -45,7 +44,7 @@
 		else if($_SESSION["accttype"] == 2){
 			$update = pg_query_params($db_conn, "UPDATE savings SET s_bal = $1 WHERE account_number = $2", array($_SESSION['bal'], $_SESSION["account_number"]));
 			header("location: /home.php");
-	/}
+		}
 
 	}
 	 
@@ -57,8 +56,20 @@
 		$val_array = array($_SESSION["account_number"]);
 		var_dump($val_array);		
 
-		$sql = "INSERT INTO transaction (account_number) VALUES ($1)";
+		$sql = "INSERT INTO transaction (account_number) VALUES ($1) RETURNING transaction_id";
 		$query = pg_query_params($db_conn, $sql, $val_array);
+
+		//updating deposit table;
+		$row = pg_fetch_row($query);
+		$trans_id = $row['0'];
+
+		$val_array = array($amount, $trans_id);
+		var_dump($val_array);		
+
+		$sql = "INSERT INTO withdraw (withdrawn, transaction_id) VALUES ($1, $2)";
+		$query = pg_query_params($db_conn, $sql, $val_array);
+
+		//updating the account
 
 		if($_SESSION["accttype"] == 1){
             $update = pg_query_params($db_conn, "UPDATE checkings SET c_bal = $1 WHERE account_number = $2", array($_SESSION['bal'], $_SESSION["account_number"]));
@@ -70,6 +81,7 @@
 			header("location: /home.php");
 		}
 	}
+
 	else if(isset($_POST['transfer'])) {	
 		$amount = $_POST['amount'];
 		$acct = $_POST['to'];
@@ -79,7 +91,17 @@
 		$val_array = array($_SESSION["account_number"]);
 		var_dump($val_array);		
 
-		$sql = "INSERT INTO transaction (account_number) VALUES ($1)";
+		$sql = "INSERT INTO transaction (account_number) VALUES ($1) RETURNING transaction_id";
+		$query = pg_query_params($db_conn, $sql, $val_array);
+
+		//updating deposit table;
+		$row = pg_fetch_row($query);
+		$trans_id = $row['0'];
+
+		$val_array = array($amount, $acct, $trans_id);
+		var_dump($val_array);		
+
+		$sql = "INSERT INTO transfer (amount, dst_account, transaction_id) VALUES ($1, $2, $3)";
 		$query = pg_query_params($db_conn, $sql, $val_array);
 
 		//subtract amount from acct
